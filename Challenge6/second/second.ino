6ce47cff0bcf6a01a5adca798aa8fe9828b88142
Luisa Garcia <luisabety200797@gmail.com>
2022-02-25 09:53:50 -0600

    Update second.ino

--- Challenge6/second/second.ino
+++ Challenge6/second/second.ino
@@ -1,21 +1,20 @@
-float var_volt; 
+float var_voltage; 
 int red_led = 7;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(red_led, OUTPUT);
+  Serial.begin(9600);//starts serial monitor
+  pinMode(red_led, OUTPUT);//starting red led at pin7
   
 }
 
 void loop() {
-  
-  var_volt = analogRead(A0)* 0.004882125;
-  float distance = 13 * pow(var_volt, -1) ;
-  Serial.println(distance);
-  if(distance>=12 && distance<=15){
+  var_voltage = analogRead(A0)*0.004882125; //voltage returned by infrared analog sensor
+  int distance = 13 * pow(var_volt, -1) ;//math equation to transform voltage to distance in centimeres(cm)
+  Serial.println(distance);//prints distance to monitor
+  if(distance>=12 && distance<=15){//if the distance is larger or equals than 12 and smaller or equals than 15
     digitalWrite(red_led, HIGH);
   }else{
     digitalWrite(red_led, LOW);
-    }
+  }
   delay(1000);
 }

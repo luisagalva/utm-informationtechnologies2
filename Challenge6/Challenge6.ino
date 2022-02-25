a074761bc5200a4c98947fc3b3c7eb6f79468382
Luisa Garcia <luisabety200797@gmail.com>
2022-02-25 09:54:02 -0600

    Update Challenge6.ino

--- Challenge6/Challenge6.ino
+++ Challenge6/Challenge6.ino
@@ -1,27 +1,20 @@
-int second = 1000;
-int button1 = 2;
-int button2 = 3;
-int button3 = 4;
-int sensor = 5;
-int red_led = 6;
-int tempsensor = analogRead(A0);
+float var_voltage; 
+int red_led = 7;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);
-  pinMode(button3, INPUT);
-  pinMode(sensor, INPUT);
-  pinMode(red_led, OUTPUT);
-
+  Serial.begin(9600);//starts serial monitor
+  pinMode(red_led, OUTPUT);//starting red led at pin7
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (digitalRead(sensor) == HIGH) {
+  var_voltage = analogRead(A0)*0.004882125; //voltage returned by infrared analog sensor
+  int distance = 13 * pow(var_volt, -1) ;//math equation to transform voltage to distance in centimeres(cm)
+  Serial.println(distance);//prints distance to monitor
+  if(distance>=12 && distance<=15){//if the distance is larger or equals than 12 and smaller or equals than 15
     digitalWrite(red_led, HIGH);
   }else{
     digitalWrite(red_led, LOW);
-    }
-
+  }
+  delay(1000);
 }
